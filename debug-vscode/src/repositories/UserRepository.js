007c608f4124fbd36f29193e79616b769c8145b0
Thays <thays.santos@omeletecompany.com>
2021-05-16 12:33:22 -0300

    Debug de aplicações node com o VSCode

---
+++ debug-vscode/src/repositories/UserRepository.js
@@ -0,0 +1,10 @@
+const users = ['Amanda', 'Bia', 'Carlos', 'Diego', 'Eliane'];
+
+export function getUsers(){
+    // return users;
+    return []
+}
+
+export function addUser(users, user){
+    users = [...users, user]
+}
